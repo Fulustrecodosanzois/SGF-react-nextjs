8a0bff408cee7258dcdb6c67e1c14ceec2216c91
luciano <luciano_jet@hotmail.com>
2025-01-08 19:41:55 -0300

    ajustado ambinete de trabaho

---
+++ sgf/app/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+
+    <div>
+
+      <div>
+        <Image src="string" />
+      </div>
+
+      <div>
+
+      </div>
+
+      <div>
+
+      </div>
+
+    </div>
+
+  );
+}
